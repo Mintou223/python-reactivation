f8400a7d6426d08530330c12b3d92d8a1b88b27d
Mintou <mintoulouiskone@gmail.com>
2026-09-24 06:20:19 -0400

    Handle empty list and short circuit in parallel resistance

--- exercices/01_resistances.py
+++ exercices/01_resistances.py
@@ -3,11 +3,20 @@ def serie (resistances):
     return round(r_eq,2)
 
 def parallele(resistances):
+    nb=len(resistances)
     invr_eq = 0
-    for r in resistances:
-        invr_eq = invr_eq + (1/r)
-    return round(1/invr_eq,2)
+    if nb == 0:
+        raise ValueError("No resistances provided.")
+    else:
+        for r in resistances:
+            if r==0:
+                return 0
+            else:
+                invr_eq = invr_eq + (1/r)
+        return round(1/invr_eq,2)
 
 print("Resistances in series:", serie([100,220,470]), "Ohms")
 print("Resistances in parallele:", parallele([100,220,470]), "Ohms")
+print("Resistances in parallele:", parallele([100,220,0]), "Ohms")
+print("Resistances in parallele:", parallele([]), "Ohms")
 
